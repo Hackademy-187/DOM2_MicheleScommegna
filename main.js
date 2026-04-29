7e6d022bf0ebc730d04f6ba8cf77f00d0b696e85
Nicola Menonna <nicola.menonna@aulab.it>
2026-04-29 09:28:03 +0200

    init project rubrica

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+    Rappresentare una rubrica che contiene una lista di contatti.
+
+    Implementare le seguentio funzionalita':
+    1- Mostrare o nascondere la lista dei contatti
+    2- Popolare la tabella rubrica in modo dinamico
+    3- Aggiungere un nuovo conttatto
+    4- Eliminare un contatto
+*/
